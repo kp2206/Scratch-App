cba3e26df798403b94a1b19f94e2c6921ec6756a
Kriti <kritip105@gmail.com>
2022-10-19 20:08:59 +0530

    adding files

--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "tailwindcss/tailwind.css";
+import GUI from './containers/gui.jsx';
+import AppStateHOC from './lib/app-state-hoc.jsx';
+import GuiReducer, {guiInitialState, guiMiddleware, initEmbedded, initFullScreen, initPlayer} from './reducers/gui';
+import LocalesReducer, {localesInitialState, initLocale} from './reducers/locales';
+import {ScratchPaintReducer} from 'scratch-paint';
+import {setFullScreen, setPlayer} from './reducers/mode';
+import {remixProject} from './reducers/project-state';
+import {setAppElement} from 'react-modal';
+import totallyNormalStrings from './lib/l10n.js';
 
-console.log("hi");
+const guiReducers = {
+    locales: LocalesReducer,
+    scratchGui: GuiReducer,
+    scratchPaint: ScratchPaintReducer
+};
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+export {
+    GUI as default,
+    AppStateHOC,
+    setAppElement,
+    guiReducers,
+    guiInitialState,
+    guiMiddleware,
+    initEmbedded,
+    initPlayer,
+    initFullScreen,
+    initLocale,
+    localesInitialState,
+    remixProject,
+    setFullScreen,
+    setPlayer,
+    totallyNormalStrings
+};
